84cf42a40aa4a0cbab2cb99202c3807997fe586a
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-28 18:00:15 +0100

    EDit import component on app

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    data() {
+        return {
+            mainTitle: "La mia prima app con Vite!"
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1  class="main_title">
+            {{ mainTitle }}
+        </h1>
+    </div>
+</template>
+
+<style>
+    .main_title{
+        position: absolute;
+        top: 30%;
+        left: 50%;
+        transform: translate(-50% -50%);
+    }
+</style>
